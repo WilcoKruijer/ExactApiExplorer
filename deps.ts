f33349f311ed265e924d55650648e77380239075
Wilco Kruijer <wilcokruijer@ziggo.nl>
2021-01-13 16:02:12 +0100

    Deps: bump Cliffy to 0.17.0 and use Nest

--- deps.ts
+++ deps.ts
@@ -7,21 +7,14 @@ export {
   prompt,
   Select,
   Toggle,
-} from "https://raw.githubusercontent.com/c4spar/deno-cliffy/f2d8c93dc1b0339bce4bbb6a6a3b80faecb38640/prompt/mod.ts";
+} from "https://x.nest.land/cliffy@0.17.0/prompt/mod.ts";
 
-// } from "https://x.nest.land/cliffy@0.16.0/prompt/mod.ts";
-
-// TODO: do not use Github as source
 export {
   ansi,
   colors,
   link,
-} from "https://raw.githubusercontent.com/c4spar/deno-cliffy/f2d8c93dc1b0339bce4bbb6a6a3b80faecb38640/ansi/mod.ts";
+} from "https://x.nest.land/cliffy@0.17.0/ansi/mod.ts";
 
-// export {
-//   plainToClass,
-//   Expose
-// } from 'https://cdn.skypack.dev/class-transformer';
 
 export { DB, Status } from "https://deno.land/x/sqlite@v2.3.2/mod.ts";
 export { default as SqliteError } from "https://deno.land/x/sqlite@v2.3.2/src/error.ts";
